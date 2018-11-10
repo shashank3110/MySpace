d6879bd2b5a96b81f5c679176175a7ac79f710b3
Shashank Salian <shashank311095@gmail.com>
2018-11-11 01:24:57 +0530

    Updated

--- aspect_ratio.py
+++ aspect_ratio.py
@@ -4,15 +4,15 @@ x=[1,2,5,7]
 y=[2,8,10,3]
 fig_data={
 'title':["original figure","Median-Absolute-Slope \n Banking","Average-Absolute-Orientation \n Banking","Weighted Average-Absolute-Orientation \n Banking"],
-'aspect_ratio':[0.5,8/21,0.434654,0.7667693]
+'aspect_ratio':[1,8/21,0.434654,0.7667693]
 }
 
 f,plots=plt.subplots(1,4)
 
 for i,p in enumerate(plots):
     p.axes.set_aspect(aspect=fig_data['aspect_ratio'][i])
-    if i==0: p.set_ylabel('y')
-    p.set_xlabel('x')
+    if i==0: p.set_ylabel('y-axis')
+    p.set_xlabel('X-axis \n aspect ratio={}'.format(fig_data['aspect_ratio'][i]))
     p.set_title(fig_data['title'][i])
     p.plot(x,y)
 
